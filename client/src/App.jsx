49bdc4b1567329e2ee7a4dbb9bf46dd460925d59
Anneice Manzanares <mountains.stars@gmail.com>
2023-03-29 13:12:00 -0600

    connected new components to app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,36 @@
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import MyNavBar from './components/Navbar'
-import ListStudents from './components/ListStudents'
-
+import './App.css';
+import { useState } from "react";
+import WeatherForm from './components/weatherForm';
+import WeatherCard from './components/weatherCard';
 
 function App() {
+  const [city, setCity] = useState("");
+  const [result, setResult] = useState("");
+
+  //A function to do the get request and set the state from the hard code data
+  const loadCity = (city) => {
+    const params = new URLSearchParams({ cityName: city });
+    fetch(`http://localhost:8080/api/weather?${params}`)
+      .then((response) => response.json())
+      .then((result) => {
+        //console.log(result)
+        setCity(city);
+        setResult(result);
+      });
+  }
+
+ const handleSubmit = (city) =>{
+  // e.preventDefault();
+  loadCity(city);
+ }
+
 
   return (
     <div className="App">
-      <MyNavBar />
-      <ListStudents />
-
+      <WeatherForm city={city} handleSubmit={handleSubmit}/>
+      {!result ? <p>Press Submit to see the weather.</p> : <WeatherCard data={result} /> }
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
